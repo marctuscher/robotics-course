c102fda3e2818d9ecdfabfbcdd92e35d0fbaafec
ralf gulde <ralf.gulde@online.de>
2019-04-24 22:13:33 +0200

    fixed hough tfrom

--- practical/cvmagic.py
+++ practical/cvmagic.py
@@ -18,7 +18,7 @@ from practical.vision import computeHoughsTransform
 #%%
 os.getcwd()
 #%%
-img = cv2.imread('practical/cvtest.jpg')
+img = cv2.imread('practical/ball3.jpg')
 
 #%%
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
@@ -33,22 +33,33 @@ mask2 = cv2.inRange(img_hsv, (170, 70, 70), (180, 255, 255))
 mask = mask1 | mask2
 
 #%%
-plt.imshow(mask)
+plt.imshow(mask, cmap='gray', vmin=0, vmax=1)
 
 
 #%%
-circles = computeHoughsTransform(mask, None)
+circles = circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1,45, param1=150,param2=13,minRadius=3,maxRadius=50)
+#(mask, None)
 
 #%%
 circles = np.uint16(np.around(circles))
-for i in circles[0,:]:
+img_ = img
+'''
+for i in [circles[0,:]]:
     # draw the outer circle
     cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
     # draw the center of the circle
     cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+'''
+u = circles[0,0,0]
+v = circles[0,0,1]
+r = circles[0,0,2]
+# draw the outer circle
+cv2.circle(img_,(u,v),r,(0,255,0),2)
+# draw the center of the circle
+cv2.circle(img_,(u,v),2,(0,0,255),3)
 
 print('show detected circles')
-plt.imshow(mask, cmap='gray', vmin=0, vmax=255)
+plt.imshow(img_, cmap='gray', vmin=0, vmax=255)
 
 
 
