72a297278a3d38205159e8af61c0809628bc50e7
Marc Tuscher <marc.tuscher@gmail.com>
2019-04-24 21:32:28 +0200

    cv stuff

--- practical/cvmagic.py
+++ practical/cvmagic.py
@@ -13,6 +13,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from practical.vision import computeHoughsTransform
 
 #%%
 os.getcwd()
@@ -26,13 +27,28 @@ plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
 #%%
-mask1 = cv2.inRange(img_hsv, (0, 70, 50), (10, 255, 255))
-mask2 = cv2.inRange(img_hsv, (170, 70, 50), (180, 255, 255))
+mask1 = cv2.inRange(img_hsv, (0, 70, 70), (10, 255, 255))
+mask2 = cv2.inRange(img_hsv, (170, 70, 70), (180, 255, 255))
 
 mask = mask1 | mask2
 
 #%%
 plt.imshow(mask)
+
+
+#%%
+circles = computeHoughsTransform(mask, None)
+
+#%%
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
+
+print('show detected circles')
+plt.imshow(mask, cmap='gray', vmin=0, vmax=255)
 
 
 
--- practical/vision.py
+++ practical/vision.py
@@ -9,8 +9,6 @@ import numpy as np
 
 
 def computeHoughsTransform(rgb, depth):
-    graying = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
-    blurred = cv2.GaussianBlur(graying,(3,3), 0)
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT,1,20, param1=50,param2=30,minRadius=0,maxRadius=0)
-    print(circles)
+    blurred = cv2.GaussianBlur(rgb,(3,3), 0)
+    circles = cv2.HoughCircles(rgb, cv2.HOUGH_GRADIENT, 1, rgb.shape[0]/8, param1=10, param2=5, minRadius=0, maxRadius=0)
     return circles
