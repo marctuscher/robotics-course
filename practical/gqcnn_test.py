be9dbdac642e356c90b7921a9928c3cd56c2fee2
ralfgulde <ralf.gulde@online.de>
2019-05-21 20:45:14 +0200

    merge

--- practical/gqcnn_test.py
+++ practical/gqcnn_test.py
@@ -13,19 +13,16 @@ except ValueError:
 import cv2
 sys.path.append('../')
 
+
 #%%
 from practical.raiRobot import RaiRobot
-from practical.rosComm import RosComm
 from practical.objectives import moveToPosition, gazeAt, scalarProductXZ, scalarProductZZ, distance
 from practical.vision import findBallPosition, findBallInImage, virtCamIntrinsics as intr
 from practical import utils
 import libry as ry
 
+
 #%%
-#ROS-related
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-#GQCNN
 from autolab_core import YamlConfig
 from gqcnn.grasping import Grasp2D, SuctionPoint2D, CrossEntropyRobustGraspingPolicy, RgbdImageState
 from gqcnn.utils import GripperMode, NoValidGraspsException
@@ -38,17 +35,7 @@ robot =  RaiRobot('', 'rai-robotModels/baxter/baxter_new.g')
 cam = ry.Camera("test", "/camera/color/image_raw/", "/camera/depth/image_rect_raw/")
 
 #%%
-rosco = RosComm('qgcnn')
-
-#%%
-#ts.signalMessage()
-rgb = rosco.latest_depth
-d = rosco.latest_rgb
-print(rgb)
-#%%
-#cfg = YamlConfig('practical/cfg/tst.yaml')
-cfg = YamlConfig('practical/cfg/examples/replication/dex-net_4.0_fc_pj.yaml')
-print(cfg)
+cfg = YamlConfig('practical/cfg/fcgqcnn_pj.yaml')
 #%%
 grasp_policy = CrossEntropyRobustGraspingPolicy(cfg['policy'])
 #%%
@@ -56,20 +43,23 @@ img = cam.getRgb()
 d = cam.getDepth()
 #%%
 plt.imshow(img)
+#%%
+plt.imshow(d)
 
 #%%
 cam_intr = CameraIntrinsics(frame='pcl', fx=intr['fx'], fy=intr['fy'], cx=intr['px'], cy=intr['py'], height=intr['height'], width=intr['width'])
-color_im = ColorImage(img.astype(np.uint8), encoding="rgb8", frame='pcl')
+color_im = ColorImage(img.astype(np.uint8), encoding="bgr8", frame='pcl')
 depth_im = DepthImage(d.astype(np.float32), frame='pcl')
 color_im = color_im.inpaint(rescale_factor=cfg['inpaint_rescale_factor'])
 depth_im = depth_im.inpaint(rescale_factor=cfg['inpaint_rescale_factor'])
 rgbd_im = RgbdImage.from_color_and_depth(color_im, depth_im)
-rgbd_state = RgbdImageState(rgbd_im, cam_intr)
+rgbd_state = (rgbd_im, cam_intr)
+
+
 
 #%%
 grasp = grasp_policy(rgbd_state)
 #%%
-
 img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.circle(img2,(int(grasp.grasp.center.x),int(grasp.grasp.center.y)),2,(255,0,0),3)
 plt.imshow(img2)
@@ -80,9 +70,7 @@ grasp.grasp.pose()
 grasp.grasp.center.x
 
 #%%
-grasp.grasp.center.x
+grasp.grasp.center.y
 
 #%%
 
-
-#%%
