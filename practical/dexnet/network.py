9f50d83ab0fd80ec5fa15fd378ad7cf03ee7829a
Marc Tuscher <marc.tuscher@gmail.com>
2019-05-25 23:24:47 +0200

    changes

--- practical/dexnet/network.py
+++ practical/dexnet/network.py
@@ -12,13 +12,13 @@ from gqcnn.grasping import FullyConvolutionalGraspingPolicyParallelJaw
 class GQCNNLoader():
 
 
-    def __init__(self, cfgFile="practical/cfg/gqcnn_pj.yaml"):
+    def __init__(self, cfgFile="practical/cfg/gqcnn_pj_serv.yaml"):
         self.cfg = YamlConfig(cfgFile)
         self.graspPolicy = CrossEntropyRobustGraspingPolicy(self.cfg['policy'])
 
     def rgbd2state(self, img, d, frame='pcl',rgbEncoding='bgr8', intr=baxterCamIntrinsics):
         cam_intr = CameraIntrinsics(frame=frame, fx=intr['fx'], fy=intr['fy'], cx=intr['cx'], cy=intr['cy'], height=intr['height'], width=intr['width'])
-        color_im = ColorImage(img.astype(np.uint8), encoding="bgr8", frame=frame)
+        color_im = ColorImage(img.astype(np.uint8), encoding=rgbEncoding, frame=frame)
         depth_im = DepthImage(d.astype(np.float32), frame=frame)
         color_im = color_im.inpaint(rescale_factor=self.cfg['inpaint_rescale_factor'])
         depth_im = depth_im.inpaint(rescale_factor=self.cfg['inpaint_rescale_factor'])
@@ -30,7 +30,15 @@ class GQCNNLoader():
         res = self.graspPolicy(rgbdState)
         pred = {
             "x": res.grasp.center.x,
-            "y": res.grasp.center.y
+            "y": res.grasp.center.y,
+            "angle": res.grasp.angle,
+            "q": res.q_value,
+            "approachAxis": [res.grasp.approach_axis[0], res.grasp.approach_axis[1], res.grasp.approach_axis[2]],
+            "axis": [res.grasp.axis[0], res.grasp.axis[0]],
+            "width": res.grasp.width,
+            "depth": res.grasp.depth,
+            "approachAngle": res.grasp.approachAngle
+
         }
         return pred
 
