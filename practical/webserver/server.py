3cc893b7ff064fdcb716404165b14952142401e4
Marc Tuscher <marc.tuscher@gmail.com>
2019-05-27 14:23:14 +0200

    cleanup

--- practical/webserver/server.py
+++ practical/webserver/server.py
@@ -5,11 +5,7 @@ import json
 import base64
 import sys
 sys.path.append('.')
-try:
-    sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
-except ValueError:
-    pass  # do nothing!
-import cv2
+from practical.webserver.utils import rgbdFromRequest
 from practical.dexnet.network import GQCNNLoader
 
 
@@ -27,23 +23,6 @@ def gqcnn():
     state = gqcnn_net.rgbd2state(img, d, intr=r['intr'])
     res = gqcnn_net.predict(state)
     return jsonify(res)
-
-def rgbdFromRequest(r):
-    rgb_str = r['rgb']
-    depth_str = r['d']
-    encoded = r['encoded']
-    intr = r['intr']
-    rgb_buff = base64.b64decode(rgb_str)
-    d_buff = base64.b64decode(depth_str)
-    nparr_rgb = np.frombuffer(rgb_buff, np.uint8)
-    nparr_depth = np.frombuffer(d_buff, np.float32)
-    if encoded:
-        img_rgb = cv2.imdecode(nparr_rgb, cv2.IMREAD_COLOR)
-    else:
-        img_rgb = np.reshape(nparr_rgb, (intr["height"], intr["width"], 3))
-    #img_d = cv2.imdecode(nparr_depth, cv2.IMREAD_ANYDEPTH)
-    nparr_depth = np.reshape(nparr_depth, (intr["height"], intr["width"]))
-    return img_rgb, nparr_depth
 
 
 if __name__ == '__main__':
---
+++ practical/webserver/utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+import base64
+import sys
+sys.path.append('.')
+import cv2
+
+
+def rgbdFromRequest(r):
+    img_rgb = rgbFromRequest(r)
+    depth_str = r['d']
+    intr = r['intr']
+    d_buff = base64.b64decode(depth_str)
+    nparr_depth = np.frombuffer(d_buff, np.float32)
+    nparr_depth = np.reshape(nparr_depth, (intr["height"], intr["width"]))
+    return img_rgb, nparr_depth
+
+def rgbFromRequest(r):
+    rgb_str = r['rgb']
+    encoded = r['encoded']
+    intr = r['intr']
+    rgb_buff = base64.b64decode(rgb_str)
+    nparr_rgb = np.frombuffer(rgb_buff, np.uint8)
+    if encoded:
+        img_rgb = cv2.imdecode(nparr_rgb, cv2.IMREAD_COLOR)
+    else:
+        img_rgb = np.reshape(nparr_rgb, (intr["height"], intr["width"], 3))
+    return img_rgb
